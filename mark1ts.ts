8987f04c1988c1fb0f1a2dece9d4e16f770ad4c1
Santosh.Mhatre <santosh.mhatre93@gmail.com>
2024-04-20 00:05:22 +0530

    optional parameters in ts

--- mark1ts.ts
+++ mark1ts.ts
@@ -51,3 +51,14 @@ console.log(students);
 
 students.pop();
 console.log(students);
+
+let Adder = (num1:number,num2:number,num3?:number):number =>{
+    if (num3!== undefined) {
+        return num1+num2+num3;
+    }
+    else{
+        return num1+num2;
+    }
+}
+
+console.log(Adder(5,4));
